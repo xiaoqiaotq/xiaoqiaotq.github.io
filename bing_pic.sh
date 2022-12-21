010279c0b1f08d40c1d1c976b6e8023c6e6dea81
tony <xiaoqiaotq@gmail.com>
2022-12-21 14:42:12 +0800

    加入fancybox 图片放大

--- bing_pic.sh
+++ bing_pic.sh
@@ -1,4 +1,7 @@
 # 参考 https://allanhao.com/2022/07/19/2022-07-19-bing-daily-picture/
-RESULT=$(curl -k -s  "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
-image_url=$(echo "${RESULT}" | jq -r '.images[0].url')
-echo "image: \"https://cn.bing.com$image_url\""
+# https://blog.csdn.net/y1534414425/article/details/107513880
+RESULT=$(curl -k -s  "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8")
+Base_URL="https://cn.bing.com"
+image_url=$(echo "${RESULT}" | jq -r '"https://cn.bing.com" +  .images[].url + "\\n"')
+echo ${image_url}
+#echo "image: \"https://cn.bing.com$image_url\""
